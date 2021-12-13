bd0adfdf93f84b7947eb620a9f5420714cf26720
user name <grasstv14@gmail.com>
2021-12-13 21:03:32 +0500

    Добавьте файлы проекта.

---
+++ alg_rgr_cs/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace alg_rgr_cs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Del = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Srch = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(56, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 29);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Введите элемент";
+            // 
+            // Del
+            // 
+            this.Del.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Del.Location = new System.Drawing.Point(211, 79);
+            this.Del.Name = "Del";
+            this.Del.Size = new System.Drawing.Size(132, 47);
+            this.Del.TabIndex = 9;
+            this.Del.Text = "Удалить";
+            this.Del.UseVisualStyleBackColor = true;
+            this.Del.Click += new System.EventHandler(this.Del_Click);
+            // 
+            // Add
+            // 
+            this.Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Add.Location = new System.Drawing.Point(29, 79);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(153, 46);
+            this.Add.TabIndex = 8;
+            this.Add.Text = "Добавить";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(290, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 34);
+            this.textBox1.TabIndex = 7;
+            // 
+            // Srch
+            // 
+            this.Srch.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Srch.Location = new System.Drawing.Point(373, 79);
+            this.Srch.Name = "Srch";
+            this.Srch.Size = new System.Drawing.Size(100, 47);
+            this.Srch.TabIndex = 12;
+            this.Srch.Text = "Найти";
+            this.Srch.UseVisualStyleBackColor = true;
+            this.Srch.Click += new System.EventHandler(this.Srch_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(445, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 29);
+            this.label3.TabIndex = 13;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PapayaWhip;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.Srch);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Del);
+            this.panel1.Controls.Add(this.Add);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(525, 708);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1170, 138);
+            this.panel1.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 25);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "label4";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Info;
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(1, 708);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(529, 138);
+            this.panel2.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Del;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Srch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ alg_rgr_cs/Form1.cs
@@ -0,0 +1,441 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Rectangle = System.Drawing.Rectangle;
+
+
+namespace alg_rgr_cs
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap bmp = new Bitmap(1800, 800);
+        public Bitmap Image { get; internal set; }
+
+        AVL tree = new AVL();
+        int rad = 15;
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+        }
+        private void DrawNodes(Node cur)
+        {
+            Rectangle rect = new Rectangle(cur.x - rad, cur.y - rad, rad * 2, rad * 2);
+            Pen pen = new Pen(Color.Black, 3);
+            Font font = new Font("Arial", 15, FontStyle.Regular);
+            SolidBrush brush = new SolidBrush(Color.White);
+            g.DrawEllipse(pen, rect);
+            g.FillEllipse(brush, cur.x - rad, cur.y - rad, rad * 2, rad * 2);
+            g.DrawString((cur.data).ToString(), font, Brushes.Black, cur.x - 15, cur.y - 10);
+            if (cur.left != null)
+            {
+                DrawNodes(cur.left);
+            }
+            if (cur.right != null)
+            {
+                DrawNodes(cur.right);
+            }
+
+        }
+        private void DrawRibs(Node cur)
+        {
+            Pen p = new Pen(Color.Black, 3);
+            Point p1 = new Point(cur.x, cur.y);
+
+
+            if (cur.left != null)
+            {
+                Point p2 = new Point(cur.left.x, cur.left.y);
+                g.DrawLine(p, p1, p2);
+                DrawRibs(cur.left);
+            }
+            if (cur.right != null)
+            {
+                Point p2 = new Point(cur.right.x, cur.right.y);
+                g.DrawLine(p, p1, p2);
+                DrawRibs(cur.right);
+            }
+        }
+        private void Clear()
+        {
+            Rectangle rect = new Rectangle(0, 0, 2000, 1200);
+            Pen pen = new Pen(Color.White, 3);
+            g.DrawRectangle(pen, rect);
+            SolidBrush brush = new SolidBrush(Color.White);
+            g.FillRectangle(brush, rect);
+
+        }
+        private void Search(Node n)
+        {
+            int x = n.x;
+            int y = n.y;
+            int d = n.data;
+            Rectangle rect = new Rectangle(x - rad, y - rad, rad * 2, rad * 2);
+            Pen pen = new Pen(Color.Green, 3);
+            Font font = new Font("Arial", 15, FontStyle.Regular);
+            SolidBrush brush = new SolidBrush(Color.Green);
+            g.DrawEllipse(pen, rect);
+            g.FillEllipse(brush, x - rad, y - rad, rad * 2, rad * 2);
+            g.DrawString((d).ToString(), font, Brushes.Black, x - 15, y - 10);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label4.Text = "Работу выполнил: Чураев Ильдар Разифович \nСтудент группы ПРО-228Б\nЗадание: Напишите программу поиска, включения\nи исключения из сбалансированного поискового\nдерева (вариант 2)\n";
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(textBox1.Text);
+            tree.Add(num);
+            tree.GetDeep2(tree.root);
+            tree.GetDeep(tree.root);
+            tree.root.x = 550;
+            tree.root.y = 50;
+            Clear();
+            tree.ForDraw(tree.root);
+            DrawRibs(tree.root);
+            DrawNodes(tree.root);
+        }
+
+        private void Srch_Click(object sender, EventArgs e)
+        {
+            if (tree.root != null)
+            { 
+                Clear();
+                DrawRibs(tree.root);
+                DrawNodes(tree.root);
+                int num = Convert.ToInt32(textBox1.Text);
+                Node nd = null;
+                nd = tree.Find(num, tree.root);
+                if (nd != null)
+                {
+                    Search(nd);
+                    label3.Text = "";
+                }
+                else
+                    label3.Text = "элемент отсутствует";
+            }
+            else label3.Text = "дерево пустое";
+        }
+
+        private void Del_Click(object sender, EventArgs e)
+        {
+            if (tree.root != null)
+            {
+                int num = Convert.ToInt32(textBox1.Text);
+                tree.Delete(num);
+                tree.GetDeep2(tree.root);
+                tree.GetDeep(tree.root);
+                if ((tree.root != null))
+                {
+                    tree.root.x = 550;
+                    tree.root.y = 30;
+                    Clear();
+                    tree.ForDraw(tree.root);
+                    DrawRibs(tree.root);
+                    DrawNodes(tree.root);
+                }
+                else
+                {
+                    Clear();
+
+                }
+            }
+        }
+
+    }
+
+    public class Node
+    {
+        public int data, deep;
+        public int x, y;
+        public Node left;
+        public Node right;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+
+    public class AVL
+    {
+
+        public Node root;
+        public AVL()
+        {
+        }
+
+        public void ForDraw(Node c)
+        {
+
+            if (c.left != null)
+            {
+                if (c.deep > 2)
+                    c.left.x = c.x - 275 / (c.deep + 1);
+                else if (c.deep == 1)
+                    c.left.x = c.x - 275 / (c.deep);
+                else if (c.deep == 2)
+                    c.left.x = c.x - 275 / (c.deep);
+                c.left.y = c.y + 125;
+                ForDraw(c.left);
+            }
+            if (c.right != null)
+            {
+                if (c.deep > 2)
+                    c.right.x = c.x + 275 / (c.deep + 1);
+                else if (c.deep == 1)
+                    c.right.x = c.x + 275 / (c.deep);
+                else if (c.deep == 2)
+                    c.right.x = c.x + 275 / (c.deep);
+                c.right.y = c.y + 125;
+                ForDraw(c.right);
+            }
+        }
+        public void GetDeep(Node n)
+        {
+            if (root != null)
+            { 
+            if (n.left != null)
+            {
+                n.left.deep = n.deep + 1;
+                GetDeep(n.left);
+            }
+            if (n.right != null)
+            {
+                n.right.deep = n.deep + 1;
+                GetDeep(n.right);
+            }
+            }
+        }
+        public void GetDeep2(Node n)
+        {
+            if (root != null)
+            {
+                n.deep = 1;
+                if (n.left != null)
+                {
+                    GetDeep2(n.left);
+                }
+                if (n.right != null)
+                {
+                    GetDeep2(n.right);
+                }
+            }
+        }
+        public void Add(int data)
+        {
+            Node newItem = new Node(data);
+            if (root == null)
+            {
+                root = newItem;
+            }
+            else
+            {
+                root = RecursiveInsert(root, newItem);
+            }
+        }
+        private Node RecursiveInsert(Node current, Node n)
+        {
+            if (current == null)
+            {
+                current = n;
+                return current;
+            }
+            else if (n.data < current.data)
+            {
+                current.left = RecursiveInsert(current.left, n);
+                current = balance_tree(current);
+            }
+            else if (n.data > current.data)
+            {
+                current.right = RecursiveInsert(current.right, n);
+                current = balance_tree(current);
+            }
+            return current;
+        }
+        private Node balance_tree(Node current)
+        {
+            int b_factor = BalanceVal(current);
+            if (b_factor > 1)
+            {
+                if (BalanceVal(current.left) > 0)
+                {
+                    current = RotateLL(current);
+                }
+                else
+                {
+                    current = RotateLR(current);
+                }
+            }
+            else if (b_factor < -1)
+            {
+                if (BalanceVal(current.right) > 0)
+                {
+                    current = RotateRL(current);
+                }
+                else
+                {
+                    current = RotateRR(current);
+                }
+            }
+            return current;
+        }
+        public void Delete(int target)
+        {
+            root = Delete(root, target);
+        }
+        private Node Delete(Node current, int target)
+        {
+            Node parent;
+            if (current == null)
+            { return null; }
+            else
+            {
+                if (target < current.data)
+                {
+                    current.left = Delete(current.left, target);
+                    if (BalanceVal(current) == -2)
+                    {
+                        if (BalanceVal(current.right) <= 0)
+                        {
+                            current = RotateRR(current);
+                        }
+                        else
+                        {
+                            current = RotateRL(current);
+                        }
+                    }
+                }
+                else if (target > current.data)
+                {
+                    current.right = Delete(current.right, target);
+                    if (BalanceVal(current) == 2)
+                    {
+                        if (BalanceVal(current.left) >= 0)
+                        {
+                            current = RotateLL(current);
+                        }
+                        else
+                        {
+                            current = RotateLR(current);
+                        }
+                    }
+                }
+                else
+                {
+                    if (current.right != null)
+                    {
+                        parent = current.right;
+                        while (parent.left != null)
+                        {
+                            parent = parent.left;
+                        }
+                        current.data = parent.data;
+                        current.right = Delete(current.right, parent.data);
+                        if (BalanceVal(current) == 2)
+                        {
+                            if (BalanceVal(current.left) >= 0)
+                            {
+                                current = RotateLL(current);
+                            }
+                            else { current = RotateLR(current); }
+                        }
+                    }
+                    else
+                    {   
+                        return current.left;
+                    }
+                }
+            }
+            return current;
+        }
+    
+        public Node Find(int target, Node current)
+        {
+            if (current != null)
+            { 
+            if (target < current.data)
+            {
+                if (target == current.data)
+                {
+                    return current;
+                }
+                else
+                    return Find(target, current.left);
+            }
+            else
+            {
+                if (target == current.data)
+                {
+                    return current;
+                }
+                else
+                    return Find(target, current.right);
+            }
+            }
+            else return null;
+
+        }
+      
+        private int IsMax(int l, int r)
+        {
+            return l > r ? l : r;
+        }
+        private int GetHeight(Node current)
+        {
+            int height = 0;
+            if (current != null)
+            {
+                int l = GetHeight(current.left);
+                int r = GetHeight(current.right);
+                int m = IsMax(l, r);
+                height = m + 1;
+            }
+            return height;
+        }
+        private int BalanceVal(Node current)
+        {
+            int l = GetHeight(current.left);
+            int r = GetHeight(current.right);
+            int b_val = l - r;
+            return b_val;
+        }
+        private Node RotateRR(Node parent)
+        {
+            Node pivot = parent.right;
+            parent.right = pivot.left;
+            pivot.left = parent;
+            return pivot;
+        }
+        private Node RotateLL(Node parent)
+        {
+            Node pivot = parent.left;
+            parent.left = pivot.right;
+            pivot.right = parent;
+            return pivot;
+        }
+        private Node RotateLR(Node parent)
+        {
+            Node pivot = parent.left;
+            parent.left = RotateRR(pivot);
+            return RotateLL(parent);
+        }
+        private Node RotateRL(Node parent)
+        {
+            Node pivot = parent.right;
+            parent.right = RotateLL(pivot);
+            return RotateRR(parent);
+        }
+    }
+
+}
